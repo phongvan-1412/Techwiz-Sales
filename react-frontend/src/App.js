7bcdbf8b54b222f0cebb26849d48396ce635ec10
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2022-06-24 08:58:23 +0700

    create laravelapi+reactfrontend

---
+++ react-frontend/src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+class App extends Component {
+  render(){
+    return ( 
+      <Router>
+        <div className="App">
+           
+
+            
+
+            
+        </div>
+      </Router>
+
+    );
+  }
+}
+
+export default App;
