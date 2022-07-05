2a377de8c371c2ac68a3ffab2983743ed2b62b3b
phuongvu <phuongvx@greenbeli.io>
2022-07-05 14:50:35 +0700

    Update tóplider

--- react-frontend/src/user/layout/LandingPageSlider/TopSlider.js
+++ react-frontend/src/user/layout/LandingPageSlider/TopSlider.js
@@ -3,14 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { getProducts } from "../../../actions/productsActions";
-
 class TopSlider extends Component {
-  componentDidMount() {
-    this.props.getProducts();
-  }
   constructor(props) {
     super(props);
     this.next = this.next.bind(this);
@@ -24,44 +17,55 @@ class TopSlider extends Component {
   }
 
   render() {
-    const { products } = this.props;
-
     const settings = {
       dots: true,
       infinite: true,
-      speed: 500,
-      slidesToShow: 4,
+      autoplay: true,
+      autoplaySpeed :3000,
+      slidesToShow: 1,
       slidesToScroll: 1,
+      arrows: true,
+      fade: true,
     };
     return (
-      <div className="row" style={{alignSelf:'center'}}>
-        <button className="button previous col-1" onClick={this.previous} style={{alignSelf:'center',backgroundColor: '#003366', border: 'none', height:'40px',borderRadius:'10px',color:'white',fontWeight:'bold',textAlign:'center',width:'70px',padding:'0px',fontSize:'13px'}}>
-          Previous
-        </button>
-
-        <div className="col-10 panel-content" style={{objectFit:'center',objectPosition:'center',alignItems:'center'}}>
-          <Slider ref={(c) => (this.slider = c)} {...settings} >
-            {products.map((product) => (
-              <div key={product.product_SKU}>
-                <img className="page-content row-panel-img" src={require(`./tempImg/${product.product_thumbnail_name}`)} />
-              </div>
-            ))}
+      <div className="row" style={{ background: "gray"}}>
+        <div className="col-3">
+          {/* <button
+            type="button"
+            class="slick-prev"
+            style={{ backgroundColor: "red", height: "100px", width: "100px",marginLeft:"200px"}}
+            onClick={this.previous}
+          >
+            Previous
+          </button> */}
+        </div>
+        <div className="col-6">
+          <Slider ref={(c) => (this.slider = c)}{...settings}>
+            <img
+              className="page-content row-panel-img"
+              src={require(`./tempImg/banner-ecom-1920x900-loyalty-app.jpg`)}
+              style={{width: '10%', height: 'auto'}}
+            />
+            <img
+              className="page-content row-panel-img"
+              src={require(`./tempImg/EN-grab-agm-ecom-1920x900.jpg`)}
+              style={{width: '10%', height: 'auto'}}
+            />
           </Slider>
         </div>
-
-        <button className="button next col-1" onClick={this.next} style={{alignSelf:'center',backgroundColor: '#003366', border: 'none', height:'40px',borderRadius:'10px',color:'white',fontWeight:'bold',textAlign:'center',width:'70px',padding:'0px',fontSize:'13px'}}>
-          Next
-        </button>
+        <div className="col-3">
+          {/* <button
+            type="button"
+            class="slick-next"
+            style={{ backgroundColor: "red", height: "100px", width: "100px" ,marginRight:"200px"}}
+            onClick={this.next}
+          >
+            Next
+          </button> */}
+        </div>
       </div>
     );
   }
 }
-TopSlider.propTypes = {
-    getProducts: PropTypes.func.isRequired,
-};
 
-const mapStateToProps = (state) => ({
-    products: state.product.products,
-});
-
-export default connect(mapStateToProps, { getProducts })(TopSlider);
+export default TopSlider;
