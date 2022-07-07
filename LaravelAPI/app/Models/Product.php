8ee35ccc156ab29e5660a3d76b0fc9371cf07a23
phuongvu <phuongvx@greenbeli.io>
2022-07-07 15:30:14 +0700

    Create product API

---
+++ LaravelAPI/app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    public $product_id;
+    public $product_name;
+    public $category_id;
+    public $product_quatity;
+    public $product_information;
+    public $product_img_name;
+    public $product_price_per_unit;
+    public $product_status;
+}
