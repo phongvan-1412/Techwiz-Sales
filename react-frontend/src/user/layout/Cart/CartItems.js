686c8cfc96762e6f57aedebd7d12331d076035f4
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2022-07-10 11:07:29 +0700

    intergrate PayPal

---
+++ react-frontend/src/user/layout/Cart/CartItems.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Link } from "react-router-dom";
+
+import PayPal from "../../pages/Payment/PayPal";
+
+const CartItems = () => {
+  return (
+    <div
+      className="container"
+      style={{
+        marginLeft: "0px",
+        marginTop: "25px",
+        background: "gray",
+        position: "absolute",
+        width: "300px",
+      }}
+    >
+      <div>
+        <div>
+          <h3>
+            <b>YOUR PRODUCT</b>
+          </h3>
+        </div>
+        <div>sdfadfasdf</div>
+        <hr />
+        <div>
+          <h5>
+            <b>CART SUBTOTAL :</b>
+          </h5>
+          <h5>subtotal price</h5>
+        </div>
+        <div>
+          <button>
+            <Link to="/cart" replace>
+              Shipping
+            </Link>
+          </button>
+
+          {checkout ? (<PayPal />) : (<button onClick={()=> {setCheckOut(true);}}>Check Out</button>)} 
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItems;
---
+++ react-frontend/src/user/pages/Payment/PayPal.js
@@ -0,0 +1,42 @@
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const PayPal = () => {
+    const paypal = useRef();
+    useEffect(()=>{
+        window.paypal.Buttons({
+            createOrder: (data, actions, err) => {
+                return actions.order.create({
+                    intent: "CAPTURE",
+                    purchase_units: [
+                        {
+                            description: "Cool looking table",
+                            amount: {
+                                currency_code: "VND",
+                                value: 650.00
+                            }
+                        }
+                    ]
+                })
+            },
+            onApprove: async (data, actions) => {
+                const order = await (actions.order.capture())
+                console.log(order)
+            },
+            onError: (err) => {
+                console.log(err)
+            }
+        }).render(paypal.current)
+    }, [])
+
+    return (
+    <div>
+        <div ref={paypal}></div>
+    </div>
+    )
+}
+
+// PayPal.propTypes = propTypes;
+// PayPal.defaultProps = defaultProps;
+
+export default PayPal;
