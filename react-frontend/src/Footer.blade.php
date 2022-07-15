7dbb9318dd396eb62c915d279182b14f9420f85c
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2022-07-15 20:05:45 +0700

    xin loi bn Nhan, quen push cho bn

---
+++ react-frontend/src/Footer.blade.php
@@ -0,0 +1,88 @@
+<footer>
+    <div className="footer-container1">
+        <div className="footer-container1-col1">
+            <div className="contact">
+                <h4>CONTACT US</h4>
+                <a href="#">
+                    <MdLocationOn />
+                    7th floor-35/6 D5 Str-Binh Thanh Dist-HCM city
+                </a>
+                <br /><br />
+                <a href="#">
+                    <FaPhoneAlt />
+                    (+84) 99 999 9999
+                </a>
+                <br /><br />
+                <a href="#">
+                    <MdEmail /> 
+                    buffdog@gmail.com
+                </a>
+            </div>
+
+            <div className="openingtimes">
+                <h4>OPENING TIMES</h4>
+                <span>
+                    <b>Weekdays</b> 07:30 - 22:30
+                    <br />
+                    <b>Weekends</b> 07:00 - 22:30
+                </span>
+            </div>
+        </div>
+
+        <div className="footer-container1-col2">
+            <h4>ABOUT US</h4>
+            <div className="latest-blog1">
+                <span>
+                    <a href="#" className="meta-facebook">About Us</a>
+                    <br />
+                    <span id="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, est labore deleniti eligendi officiis facere.</span> 
+                    <br />
+                    <a href="/about" id="a">https://buff.ly/2zaSfAQ</a>
+                    <br />
+                    <span id="date">12 May 2022</span>
+                </span>
+            </div>
+            <br /><br />
+
+            <div className="latest-blog2">
+                <span>
+                    <a href="#" className="meta-facebook"><FaFacebook />Term and Policy</a>
+                    <br />
+                    <span id="blog">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, est labore deleniti eligendi officiis facere.</span> 
+                    <br />
+                    <a href="/term-and-policy" id="a">https://buff.ly/2zaSfAQ</a>
+                    <br />
+                    <span id="date">12 May 2022</span> 
+                </span>
+            </div>
+        </div>
+
+        <div className="footer-container1-col3">
+            <h4>GALLERY</h4>
+            <div className="gallery1">
+                <img src={{asset('../../img/Footer/image-1.jpg')}} className="gallery1__img1" />
+                <img src={{asset('../../img/Footer/image-2.jpg')}} className="gallery1__img2" />
+                <img src={{asset('../../img/Footer/image-3.jpg')}} className="gallery1__img3" />
+                <img src={{asset('../../img/Footer/image-7.jpg')}} className="gallery1__img4" />
+                <img src={{asset('../../img/Footer/image-5.jpg')}} className="gallery1__img5" />
+                <img src={{asset('../../img/Footer/image-9.jpg')}} className="gallery1__img6" />
+                <img src={{asset('../../img/Footer/image-4.jpg')}} className="gallery1__img7" />
+                <img src={{asset('../../img/Footer/image-8.jpg')}} className="gallery1__img8" />
+                <img src={{asset('../../img/Footer/image-6.jpg')}} className="gallery1__img9" />
+            </div>
+        </div>
+    </div>
+
+    <div className="footer-containter2">
+        <div className="footer-icon">
+            <a href="#"><FaFacebook /></a>
+            <a href="#"><FaInstagramSquare /></a>
+            <a href="#"><FaTwitter /></a>
+        </div>
+
+        <div id="sentence">
+            @ 2017 VNHP's Grocery. <a href="#">Get The Theme</a>
+        </div>
+    </div>
+</footer>
+
